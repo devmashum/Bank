dd3bd120d285d2da52fd2f2bc88a994691838940
devmashum <dev.mashum@gmail.com>
2023-08-16 10:35:06 +0200

    added new page

---
+++ js/login.js
@@ -0,0 +1,28 @@
+// step-1: Add click event handler with the submit button 
+
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // step-2 get the email address
+    // NOTE: always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    // step-3: get password
+    // 3.a,: set id on the html element 
+    // 3.b,: get the element
+    // 3.c,: get the value from the element
+
+    const passField = document.getElementById('user-password');
+    const password = passField.value;
+
+
+    // step 4- verify email and password and check whether valid user or not
+
+    if (email === 'faisal@mashum.com' && password === '11062016') {
+        window.location.href = 'bank.html';
+    }
+    else { alert('Your input is not valid') };
+
+
+})
+
+
+
